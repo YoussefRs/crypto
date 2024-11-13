a03d9f9ce4381023361b0005ec27ce84e2a073be
YoussefRs <rs.youssef24@gmail.com>
2024-11-13 09:55:14 +0100

    fix

--- src/global/sidebar/Sidebar.jsx
+++ src/global/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import "./Sidebar.css";
+import "./sidebar.css";
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   const sidebarRef = useRef();
